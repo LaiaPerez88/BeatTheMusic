db3f1c1055dc2c6de69b2142739638a5ec1eceb3
LaiaPerez88 <laiaperezrios88@gmail.com>
2015-09-26 14:47:19 +0100

    jquery added

---
+++ app/assets/javascripts/home.js
@@ -0,0 +1,19 @@
+/**
+ * Created by laia on 25/9/15.
+ */
+$(document).ready( function(){
+
+    alert('hello')
+    //set all functions and variables need
+    $('#rightButton').click( function(){ slideRight()});
+    $('#leftButton').click('slideLeft()');
+})
+
+
+function slideRight(i){
+    alert("slideRight");
+}
+
+function slideLeft(i){
+    alert("slideLeft");
+}
