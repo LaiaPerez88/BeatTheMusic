9e3341b8dc5c8e445458d244df6cf1adc4071409
LaiaPerez88 <laiaperezrios88@gmail.com>
2015-09-26 19:30:18 +0100

    gallery right movement

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -2,18 +2,36 @@
  * Created by laia on 25/9/15.
  */
 $(document).ready( function(){
-
-    alert('hello')
     //set all functions and variables need
-    $('#rightButton').click( function(){ slideRight()});
-    $('#leftButton').click('slideLeft()');
-})
+    $index = 0;
+
+    $('#rightButton').click( function(){ slideRight($index,2)});
+    $('#leftButton').click( function(){ slideLeft($index,2)});
+});
 
 
-function slideRight(i){
-    alert("slideRight");
+function slideRight(i,max){
+
+    if (i < max) {
+        var current = $('#item-'+i);
+        $index = i + 1;
+        var next = $('#item-' + $index);
+
+        //current.css('opacity', 0);
+        current.css('top', '0px');
+        current.css('left',-540);
+        next.css('position', 'absolute');
+        next.css('top', '0px');
+        next.css('left', '0px');
+
+    }
+    else{
+        $index = 0;
+    }
+
 }
 
 function slideLeft(i){
-    alert("slideLeft");
+
+
 }
