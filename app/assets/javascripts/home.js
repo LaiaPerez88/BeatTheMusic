6752d7fa7bd047aa5d1476c38d2eca43da9cc8e8
LaiaPerez88 <laiaperezrios88@gmail.com>
2015-09-30 17:42:46 +0100

    html and css fixed

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -2,8 +2,6 @@
  * Created by laia on 25/9/15.
  */
 $(document).ready( function(){
-    //set all functions and variables need
-    $index = 0;
 
     $('#rightButton').click( function(){ slideRight($index,2)});
     $('#leftButton').click( function(){ slideLeft($index,2)});
@@ -12,22 +10,6 @@ $(document).ready( function(){
 
 function slideRight(i,max){
 
-    if (i < max) {
-        var current = $('#item-'+i);
-        $index = i + 1;
-        var next = $('#item-' + $index);
-
-        //current.css('opacity', 0);
-        current.css('top', '0px');
-        current.css('left',-540);
-        next.css('position', 'absolute');
-        next.css('top', '0px');
-        next.css('left', '0px');
-
-    }
-    else{
-        $index = 0;
-    }
 
 }
 
